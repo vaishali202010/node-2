b1615077061cf1710fc1afb78a7c021a45202cb8
vaishali202010 <pvaishali@gmail.com>
2024-08-30 12:53:45 +0530

    @bhjb@

--- index.js
+++ index.js
@@ -75,6 +75,6 @@ app.delete('/items/:id', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${port}`);
 });
